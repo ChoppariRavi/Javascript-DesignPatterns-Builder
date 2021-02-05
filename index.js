8631f5e2ac37c9ddf9b3fb94d43115f627644e22
ChoppariRavi <choppari.ravi@gmail.com>
2021-02-05 11:35:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+
+/*let cb = new CodeBuilder('Person');
+cb.addField('name').addField('age');
+console.log(cb.toString());*/
+
+/*class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}*/
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
